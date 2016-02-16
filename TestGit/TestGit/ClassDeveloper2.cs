4fa6344beb80f4026331267f183bc2d74c289cfd
shvedskiy <shvedskiy@mail.ru>
2016-02-16 15:13:05 +0300

    Console developer 2

---
+++ TestGit/TestGit/ClassDeveloper2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    class ClassDeveloper2
+    {
+        void Message()
+        {
+            
+        }
+    }
+}
